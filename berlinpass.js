7306591d628a0b990ec843f639a1b366aec727f8
wenig <wenig@users.noreply.github.com>
2015-02-21 15:56:03 +0100

    Update berlinpass.js

--- berlinpass.js
+++ berlinpass.js
@@ -1,4 +1,12 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+document.getElementById("map").style.height = window.innerHeight+"px";
+
+//---Act-Position---
+var map = L.map('map').setView([52.520, 13.404], 13);
+if (navigator.geolocation) {
+	navigator.geolocation.getCurrentPosition(function(data){
+		map.panTo(new L.LatLng(data.coords.latitude, data.coords.longitude));
+	});
+}
 
 		L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
 			maxZoom: 18,
