50ed789001f8b8a72818bd67008c2a0b60e7812d
Phillip <s_wenigp@stud.hwr-berlin.de>
2015-02-21 16:15:36 +0100

    zoomIN
    load.js

--- load.js
+++ load.js
@@ -1,9 +1,10 @@
 document.getElementById("map").style.height = window.innerHeight+"px";
 
 //---Act-Position---
-var map = L.map('map').setView([52.520, 13.404], 13);
+var map = L.map('map').setView([52.520, 13.404], 11);
 if (navigator.geolocation) {
 	navigator.geolocation.getCurrentPosition(function(data){
 		map.panTo(new L.LatLng(data.coords.latitude, data.coords.longitude));
+		map.zoomIn(3);
 	});
 }
